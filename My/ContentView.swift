2f665a4cdb57392e34eedec0a448fe2ca4be45e6
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-04 01:56:13 +0000

    Initial commit

---
+++ My/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  My
+//
+//  Created by datttrian on 2024-01-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ My/MyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyApp.swift
+//  My
+//
+//  Created by datttrian on 2024-01-03.
+//
+
+import SwiftUI
+
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
