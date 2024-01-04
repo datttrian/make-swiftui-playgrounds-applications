0d874c733f07b98b5506a58a6db9001851d78c7c
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-03 19:33:06 -0800

    Create custom functions

--- My/ContentView.swift
+++ My/ContentView.swift
@@ -8,6 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    func pizzaSize(size:Double) -> String {
+        if size >= 25 {return "X-Large"}
+        if size >= 18 {return "Large"}
+        if size >= 12 {return "Medium"}
+        return "Small"
+    }
+    func pizzaOrderText(size:Double) -> Text {
+        Text(pizzaSize(size: size) + " Pizza")
+    }
     var bannerImage:String = surfgirl
     var color = Color.green
     var title:String
@@ -15,6 +24,7 @@ struct ContentView: View {
         VStack {
             Text(title)
                 .font(.largeTitle).bold()
+            pizzaOrderText(size: 17).font(.largeTitle)
             Image(bannerImage)
                 .resizable()
                 .scaledToFit()
