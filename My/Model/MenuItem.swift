fbe85f016aaa377173e9b9b041d2d956473deb16
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-08 18:21:47 -0800

    Importing models and assets

--- My/Model/MenuItem.swift
+++ My/Model/MenuItem.swift
@@ -11,6 +11,8 @@ struct MenuItem: Identifiable {
     var id:Int
     var name:String
     var price:Double
+    var description:String = "Description here"
+    var rating:Int = 3
     var imageName: String {
         return "\(id).circle"
     }
@@ -19,10 +21,17 @@ struct MenuItem: Identifiable {
     }
 }
 
-let testMenuItem1 = MenuItem(id: 1, name: "Cheese", price: 7.00)
+let testMenuItem1 = MenuItem(
+    id: 0,
+    name: "Margherita",
+    price: 12.99,
+    description: "The classic pizza of Buffalo. Mozzarella, tomatoes, and basil on a Neopolitan crust",
+    rating: 5
+)
+
 let testMenuItem2 = MenuItem(id: 2, name: "Pepperoni", price: 9.00)
 
-class MenuModel {
+class OldMenuModel {
     var menu: [MenuItem] = [
         testMenuItem1,
         testMenuItem2
